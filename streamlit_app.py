79cb718b397641798e30b8faf789b2d4c8ced86a
wjbsol <117731519+wjbsol@users.noreply.github.com>
2023-01-04 01:06:02 +0000

    Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 # add stop
-steamlit.stop()
+streamlit.stop()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
